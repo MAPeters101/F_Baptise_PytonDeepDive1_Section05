5801a9d9a68bcd6ef5829bbac62d639d72dffae1
Mark Peters <mapeters101@earthlink.net>
2024-11-07 10:07:13 -0500

    Lesson066:  Work on lesson from 3:00 to 12:20.

--- lesson066.py
+++ lesson066.py
@@ -33,8 +33,80 @@ print(a)
 # a = (,)
 # print(type(a))
 # print(a)
+print('='*30)
 
+a, b, c = [1, 'a', 3.14]
+print(a)
+print(b)
+print(c)
+print()
 
+(a, b, c) = [1, 2, 3]
+print(a)
+print(b)
+print(c)
+print()
+
+a, b = (1, 2)
+print(a)
+print(b)
+print()
+
+(a, b) = (1, 2)
+print(a)
+print(b)
+print()
+
+a, b = 10, 20
+print(a)
+print(b)
+print()
+
+a, b, c = 10, 'a', 3.14
+print(a)
+print(b)
+print(c)
+print()
+
+a, b, c = 10, {1, 2}, ['a', 'b']
+print(a)
+print(b)
+print(c)
+print()
+
+a, b = 10, 20
+print(a)
+print(b)
+a, b = b, a
+print(a)
+print(b)
+print()
+print('+'*30)
+
+for e in 'XYZ':
+    print(e)
+print()
+
+a, b, c = 'XYZ'
+print(a)
+print(b)
+print(c)
+print()
+
+s = 'XYZ'
+print(s[0])
+print(s[1])
+print(s[2])
+print()
+
+s = {1, 2, 3}
+# print(s[0])
+# print(s[1])
+# print(s[2])
+print()
+
+s = {'p', 'y', 't', 'h', 'o', 'n'}
+print(s)
 
 
 
