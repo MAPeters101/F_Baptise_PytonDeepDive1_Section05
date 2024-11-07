82840ee8cfa96acd08d1b208f00e1a338698f0f0
Mark Peters <mapeters101@earthlink.net>
2024-11-07 10:28:47 -0500

    Lesson066:  Complete work on lesson from 17:20.

--- lesson066.py
+++ lesson066.py
@@ -158,4 +158,36 @@ print(c)
 print(d)
 print()
 
+print('='*40)
+d = {'a':1, 'b':2, 'c':3, 'd':4}
+for e in d:
+    print(e)
+print()
+
+for e in d.values():
+    print(e)
+print()
+
+a,b,c,d = d.values()
+print(a)
+print(b)
+print(c)
+print(d)
+print()
+
+dict1 = {'a':1, 'b':2, 'c':3, 'd':4}
+for e in dict1.items():
+    print(e)
+print()
+
+for e in dict1.items():
+    a, b = e
+    print('key={0}, value={1}'.format(a, b))
+print()
+
+for a, b in dict1.items():
+    print('key={0}, value={1}'.format(a, b))
+print()
+
+
 
