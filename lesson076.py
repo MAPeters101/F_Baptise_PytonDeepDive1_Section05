669afa82c9a374a8465a17008d0033d4c5ffa34b
Mark Peters <mapeters101@earthlink.net>
2024-11-08 08:46:34 -0500

    Lesson076:  Work on lesson from 6:40 to 12:00.

--- lesson076.py
+++ lesson076.py
@@ -11,7 +11,24 @@ def time_it(fn, *args, rep=1, **kwargs):
 
 print(time_it(print, 1, 2, 3, sep=' - ', end=' ***\n', rep=5))
 
+def compute_powers_1(n, *, start=1, end):
+    # Using a for loop
+    results = []
+    for i in range(start, end):
+        results.append(n**i)
+    return results
 
+print(compute_powers_1(2, end=5))
 
+def compute_powers_2(n, *, start=1, end):
+    # Using a list comprehension
+    return [n**i for i in range(start, end)]
 
+print(compute_powers_2(2, end=5))
+
+def compute_powers_3(n, *, start=1, end):
+    # Using generator expression
+    return (n**i for i in range(start, end))
+
+print(list(compute_powers_3(2, end=5)))
 
